ab3dbe83d43e1e8bf61a06a58cf8281a8845570b
Justin Collier <justin.collier@dcmail.ca>
2024-07-05 14:28:51 -0400

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ import mysql.connector
 from urllib.parse import urlparse
 
 def get_db_connection():
-    db_url = os.environ.get('mysql://w2xyawpti7k8bp70:ujow0ny034la0elg@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c1obmzcccmpqz8pq') #JAWSBD url used for Heroku deployment
+    db_url = os.environ.get('JAWSDB_URL')  # Fetch JAWSDB_URL from environment variables
     if db_url:
         url = urlparse(db_url)
         connection = mysql.connector.connect(
@@ -15,11 +15,6 @@ def get_db_connection():
         )
         return connection
     else:
-        # Fallback to local database configuration
-        connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        database='student_db'
-    )
-    return connection
+        # handle error if connectiong can't be made
+        raise Exception('JAWSDB_URL not found in environment variables')
+
