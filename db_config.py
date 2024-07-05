ad024e8548e7b939d6b82bc744905709c07c6969
Justin Collier <justin.collier@dcmail.ca>
2024-07-05 15:00:54 -0400

    Error Handling for DB Connection

--- db_config.py
+++ db_config.py
@@ -4,17 +4,15 @@ from urllib.parse import urlparse
 
 def get_db_connection():
     db_url = os.environ.get('JAWSDB_URL')  # Fetch JAWSDB_URL from environment variables
-    if db_url:
-        url = urlparse(db_url)
-        connection = mysql.connector.connect(
-            host=url.hostname,
-            user=url.username,
-            password=url.password,
-            database=url.path[1:],
-            port=url.port
-        )
-        return connection
-    else:
-        # handle error if connectiong can't be made
+    if not db_url:
         raise Exception('JAWSDB_URL not found in environment variables')
 
+    url = urlparse(db_url)
+    connection = mysql.connector.connect(
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path[1:],
+        port=url.port
+    )
+    return connection
